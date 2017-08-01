3d5d3202bf34a74b701e1663e640624519479065
pankaj <pankajdebnath626@gmail.com>
2017-08-01 14:41:47 +0600

    dhdhth

--- wp-config-sample.php
+++ wp-config-sample.php
@@ -32,7 +32,7 @@ define('DB_PASSWORD','pankaj@');
 define('DB_HOST','mysql5.gear.host');
 
 /** Database Charset to use in creating database tables. */
-define('DB_CHARSET','utf8mp4');
+define('DB_CHARSET','utf8');
 
 /** The Database Collate type. Don't change this if in doubt. */
 define('DB_COLLATE','');
